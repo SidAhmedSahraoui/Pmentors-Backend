4b5a356bdff1b92cc96039d75075ce76ada57eec
Sid Ahmed Sahraoui <s.sahraoui@esi-sba.dz>
2023-03-03 21:51:26 +0100

    Initial commit

---
+++ AuthUsersMicroService/src/main/java/com/example/authusersmicroservice/AuthUsersMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.authusersmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthUsersMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthUsersMicroServiceApplication.class, args);
+    }
+
+}
---
+++ AuthUsersMicroService/src/test/java/com/example/authusersmicroservice/AuthUsersMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authusersmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthUsersMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
