e55a8c855a2726a72d96bc3c74eecaa9d220f8c8
Sid Ahmed Sahraoui <s.sahraoui@esi-sba.dz>
2023-03-15 16:09:02 +0100

    add service discovery & gateway

---
+++ InterviewMicroService/src/main/java/com/example/interviewmicroservice/InterviewMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.interviewmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewMicroServiceApplication.class, args);
+    }
+
+}
---
+++ InterviewMicroService/src/test/java/com/example/interviewmicroservice/InterviewMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interviewmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
